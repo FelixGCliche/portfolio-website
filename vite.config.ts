a9f87f90728d9ccd3b032761ddf6f2adff43ab33
FelixGCliche <48337098+FelixGCliche@users.noreply.github.com>
2026-06-07 01:51:54 -0400

    setup tailwind with vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import tailwindcss from '@tailwindcss/vite'
 import devtools from 'solid-devtools/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [devtools(), tanstackRouter({ target: 'solid' }), solid()],
+  plugins: [devtools(), solid(), tailwindcss(), tanstackRouter({ target: 'solid' })],
 })
