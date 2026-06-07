b7844dcc1761d60f45b8faa646c1fc3969637941
FelixGCliche <48337098+FelixGCliche@users.noreply.github.com>
2026-06-07 15:56:22 -0400

    setup color and font overrides

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,8 +6,13 @@ export const Route = createFileRoute('/')({
 
 function Home() {
   return (
-    <div class="p-2">
-      <h3>Welcome Home!</h3>
+    <div class="flex flex-col gap-4 text-xl m-2">
+      <p class="font-montreal">Whereas recognition of the inherent dignity</p>
+      <p class="font-montreal font-bold italic">Whereas recognition of the inherent dignity</p>
+      <p class="font-montreal-text">Whereas recognition of the inherent dignity</p>
+      <p class="font-montreal-text font-bold italic">Whereas recognition of the inherent dignity</p>
+      <p class="font-eiko">Whereas recognition of the inherent dignity</p>
+      <p class="font-eiko font-bold italic">Whereas recognition of the inherent dignity</p>
     </div>
   )
 }
